e3e90f34ffb37cd0734182800ae50ec2d9727ba3
orkungo <orkungo@yahoo.com>
2014-02-22 09:49:59 -0800

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Presentation::Application.config.secret_token = '189a8c1cbf1b1334eb04a701ead554bd8ba5d9002dc43e68802bdbe0050484645415a3bcfa7d224eac8767a8e53aaee73ca85290aa3dede112132453b8ba70b8'
